698b11f89553741be68970ffb34635cc68ccb195
Saurabh Barde <saurabhbarde1989@gmail.com>
2023-07-27 08:58:59 +0530

    Major Changes added

--- components/CustomerCard.jsx
+++ components/CustomerCard.jsx
@@ -4,16 +4,16 @@ const CustomerCard = (props) => {
 
     return (
         <>
-            <div className='w-[307px] px-[16px] py-[16px] h-auto border-[#fff] border-[1.5px]'>
-                <div>
+            <div className='w-[307px] px-[16px] py-[16px] h-auto border-[#fff] border-[1.5px] flex flex-col justify-center items-start gap-[10px] rounded-md'>
+                <div className='flex justify-start items-center gap-[10px]'>
                     <div className='rounded-xl w-[56px] h-[56px] '><img className=' object-cover' src="./1.png" alt="user" /></div>
-                    <div>
-                        <span></span>
-                        <span></span>
+                    <div className='flex flex-col justify-center items-start'>
+                        <span className='font-bold text-[17px]'>Brooklyn Simmons</span>
+                        <span className='text-[#D9D9D9] text-[17px]'>manam</span>
                     </div>
                 </div>
                 <div>
-                    <p className='whitespace-normal'>
+                    <p className='whitespace-normal font-light text-[15px]'>
                         Sit ut diam bibendum dolor. Ullamcorper pharetra nibh eget vitae pulvinar. Placerat sapien, dolor, aenean vivamus in tincidunt et. Mauris dolor vestibulum et lacus a ante orci.
                     </p>
                 </div>
--- components/CustomerFeedback.jsx
+++ components/CustomerFeedback.jsx
@@ -5,11 +5,14 @@ import CustomerCard from "./CustomerCard";
 const CustomerFeedback = () => {
   return (
     <>
-        <div className="w-[100vw] h-auto px-[40px] py-[40px] bg-black flex flex-col justify-center items-start gap-[20px]">
+        <div className="w-[100vw] h-auto px-[40px] py-[60px] bg-black flex flex-col justify-center items-start gap-[40px]">
             <div className="text-[30px]">
                 <h1>Hear what our customer say :&#41;</h1>
             </div>
-            <div>
+            <div className="">
+                <CustomerCard/>
+                <CustomerCard/>
+                <CustomerCard/>
                 <CustomerCard/>
             </div>
         </div>
