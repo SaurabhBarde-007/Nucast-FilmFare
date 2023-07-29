3a18f49a29eacbe19f39f83a8c62d3e86f52c7d0
Saurabh Barde <saurabhbarde1989@gmail.com>
2023-07-30 00:56:54 +0530

    "Minor Changes"

--- components/CustomerCard.jsx
+++ components/CustomerCard.jsx
@@ -4,7 +4,7 @@ const CustomerCard = ({title,subtitle,src,content}) => {
 
     return (
         <>
-            <div className='w-[70vw] sm:w-[307px] px-[16px] py-[16px] h-auto border-[#fff] border-[1.5px] flex flex-col justify-center items-start gap-[10px] rounded-md flex-shrink-0'>
+            <div id='card' className='w-[70vw] sm:w-[307px] px-[16px] py-[16px] h-auto border-[#fff] border-[1.5px] flex flex-col justify-center items-start gap-[10px] rounded-md flex-shrink-0'>
                 <div className='flex justify-start items-center gap-[10px]'>
                     <div className='rounded-xl w-[42px] h-[42px] sm:w-[52px] sm:h-[52px] '><img className=' object-cover' src={src} alt="user" /></div>
                     <div className='flex flex-col justify-center items-start'>
